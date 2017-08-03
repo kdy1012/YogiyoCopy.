4be9cd5c6dd474d256d602b3a4b01b3d88b6d94d
cho881020 <cho881020@gmail.com>
2017-08-03 10:20:43 +0900

    탭호스트 추가, 메인액티비티 변경

--- app/src/main/java/kr/co/tjeit/yogiyocopy/MainActivity.java
+++ app/src/main/java/kr/co/tjeit/yogiyocopy/MainActivity.java
@@ -1,22 +1,19 @@
 package kr.co.tjeit.yogiyocopy;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.TabActivity;
 import android.os.Bundle;
 import android.widget.ListView;
-import android.widget.Spinner;
+import android.widget.TabHost;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import kr.co.tjeit.yogiyocopy.adapter.SortSpinnerAdapter;
 import kr.co.tjeit.yogiyocopy.adapter.StoreAdapter;
 import kr.co.tjeit.yogiyocopy.data.StoreData;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends TabActivity {
 
-    private Spinner sortSpinner;
-    List<String> sortList = new ArrayList<>();
-    SortSpinnerAdapter sortSpinnerAdapter;
+    TabHost myTabHost;
 
     private ListView storeListView;
     List<StoreData> storeDataList = new ArrayList<>();
@@ -26,25 +23,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sortSpinner = (Spinner) findViewById(R.id.sortSpinner);
+        myTabHost = getTabHost();
         storeListView = (ListView) findViewById(R.id.storeListView);
 
-        sortList.add("기본 정렬순");
-        sortList.add("별점순");
-        sortList.add("리뷰 많은순");
-        sortList.add("최소 주문 금액순");
-        sortSpinnerAdapter = new SortSpinnerAdapter(MainActivity.this,
-                sortList);
-        sortSpinner.setAdapter(sortSpinnerAdapter);
+        TabHost.TabSpec spec1 = myTabHost.newTabSpec("tab1").setIndicator("가게목록");
+        spec1.setContent(R.id.tab1);
+        myTabHost.addTab(spec1);
+
+
+        TabHost.TabSpec spec2 = myTabHost.newTabSpec("tab2").setIndicator("주문내역");
+        spec2.setContent(R.id.tab2);
+        myTabHost.addTab(spec2);
+
+        TabHost.TabSpec spec3 = myTabHost.newTabSpec("tab3").setIndicator("더보기");
+        spec3.setContent(R.id.tab3);
+        myTabHost.addTab(spec3);
+
 
 
         storeDataList.add(new StoreData("교촌치킨-대학로점", 4.2f, 1200, 2330, 15000, true));
         storeDataList.add(new StoreData("원할머니보쌈-종로5가점", 3.8f, 1100, 300, 25000, false));
 
         storeAdapter = new StoreAdapter(MainActivity.this, storeDataList);
-
         storeListView.setAdapter(storeAdapter);
-
 
     }
 }
